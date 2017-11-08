fbcc408a030c986cb4f55b906980805a18103f48
Blankiushka <blankiushka@gmail.com>
2017-11-07 22:58:38 -0600

    Registrando los modelos para acceder a ellos desde el panel de administración de django.

--- Expediente/settings.py
+++ Expediente/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'es-mx'
+LANGUAGE_CODE = 'es-sv'
 
 TIME_ZONE = 'UTC'
 
