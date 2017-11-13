33d863e27f90ba86b12db8ed8504514a5388c1a7
Blankiushka <blankiushka@gmail.com>
2017-11-13 09:50:37 -0600

    Actualizando archivos estáticos

---
+++ static/js/signos-vitales/listado.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("#TablaSignosVitales").dataTable({
+        dom: "<'row'<'col-md-6'l><'col-md-6'f>>" +
+                "<'row'<'col-md-12'tr>>" +
+                "<'row'<'col-md-5'i><'col-md-7'p>>"
+    });
+});
